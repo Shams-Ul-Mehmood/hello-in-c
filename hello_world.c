2e69987f12f013a96c55fd07b35fb3d4bb0129a5
Shams-Ul-Mehmood <161685985+Shams-Ul-Mehmood@users.noreply.github.com>
2025-07-31 22:35:43 +0500

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,5 @@
+// hello.c                   // customary comment of program name
+#include <stdio.h>           // needed for screen printing
+int main(){                  // required main function
+  printf("Hello haxor!\n");  // simply say hello
+}                            // exit program
